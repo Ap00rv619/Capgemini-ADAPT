799c39bfdf240c6b8a03213eb96880b391280a7b
Ap00rv619 <75066896+Ap00rv619@users.noreply.github.com>
2021-02-20 23:12:13 +0530

    Create 1.sql

---
+++ RDBMS/Single-Row-Function/1.sql
@@ -0,0 +1,5 @@
+alter session set current_schema=bank;
+SET heading OFF;
+SELECT CUST_ID, UPPER(CUST_FNAME), UPPER(CUST_LNAME), TO_CHAR(CUST_DOB, 'DD--Mon') AS DOB
+FROM BANK_CUSTOMER
+WHERE CUST_SEX = 'F';
