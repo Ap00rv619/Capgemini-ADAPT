f4c25cdb79e13299d903a15914ccfbd6633de295
Ap00rv619 <75066896+Ap00rv619@users.noreply.github.com>
2021-02-25 23:25:08 +0530

    Create 3.sql

---
+++ Case Study Sprint1/Learning Outcome1/3.sql
@@ -0,0 +1,9 @@
+alter session set current_schema = bookrental;
+SET heading OFF
+/*
+ * Enter your query below.
+ * Please append a semicolon ";" at the end of the query
+ */
+ select * 
+ from user_details
+ order by first_name;
